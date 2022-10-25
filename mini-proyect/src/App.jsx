80d3097c4e7d548cf6ebbbc7bb3d45604c122966
Iris-Rf <irys@hotmail.com>
2022-10-25 16:52:19 +0200

    mini-proyect

---
+++ mini-proyect/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { CharacterList } from "./components/CharacterList";
+
+const App = () => {
+  return <CharacterList />;
+};
+
+export default App;
